d1657aa06fd000bdfcdee096152c8f4c0515a6ee
threedowon <3dowon@gmail.com>
2025-12-18 23:43:06 +0900

    project init / leapmotion plugin

---
+++ ShadowPuppets/Source/ShadowPuppets/ShadowPuppets.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShadowPuppets.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShadowPuppets, "ShadowPuppets" );
